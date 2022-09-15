7add3c70e89563ce247d6e6bfed2ba4c79a87066
segelt <86981324+segelt@users.noreply.github.com>
2022-09-16 00:57:29 +0300

    include user handler

---
+++ internal/snippetdemo/handler/userhandlers.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"snippetdemo/internal/snippetdemo/service"
+)
+
+type UserHandler struct {
+	svc service.UserService
+}
+
+func NewUserHandler(svc service.UserService) *UserHandler {
+	h := UserHandler{svc: svc}
+
+	return &h
+}
+
+func (h *UserHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	type UserRegisterReq struct {
+		Username string
+		Password string
+	}
+
+	var req UserRegisterReq
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.svc.RegisterUser(req.Username, req.Password)
+
+	if err != nil {
+		render(w, err, http.StatusInternalServerError)
+	}
+
+	render(w, "Success", http.StatusCreated)
+}
