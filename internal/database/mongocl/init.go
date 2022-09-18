7d0e6eb083f5def687bcfc2da2c4524de59e1623
segelt <86981324+segelt@users.noreply.github.com>
2022-09-18 05:28:21 +0300

    seed category

--- internal/database/mongocl/init.go
+++ internal/database/mongocl/init.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -37,6 +38,40 @@ func (s *Repo) Initialize(uri string) error {
 	if err != nil {
 		log.Fatal(err)
 		log.Fatal("Could not connect to db. Terminating app")
+		return err
+	}
+
+	err = s.seedCategories()
+
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("Could not seed categories.")
+		return err
+	}
+
+	log.Println("Data seeded.")
+
+	return nil
+}
+
+func (s *Repo) seedCategories() error {
+	coll := s.Client.Database("snippetdb").Collection("categories")
+
+	filter_testcategory1 := bson.D{{"categoryId", 1}}
+	update_testcategory1 := bson.D{{"$set", bson.D{{"categoryId", 1}, {"description", "testcategory1"}}}}
+
+	filter_testcategory2 := bson.D{{"categoryId", 2}}
+	update_testcategory2 := bson.D{{"$set", bson.D{{"categoryId", 2}, {"description", "testcategory2"}}}}
+
+	opts := options.Update().SetUpsert(true)
+
+	_, err := coll.UpdateOne(context.TODO(), filter_testcategory1, update_testcategory1, opts)
+	if err != nil {
+		return err
+	}
+
+	_, err = coll.UpdateOne(context.TODO(), filter_testcategory2, update_testcategory2, opts)
+	if err != nil {
 		return err
 	}
 
