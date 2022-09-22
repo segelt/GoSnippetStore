c7cd9522e44f8074c2000143e11d155e30d3d88d
segelt <86981324+segelt@users.noreply.github.com>
2022-09-23 00:56:04 +0300

    implement user filtering

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"context"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -18,6 +19,14 @@ func (user User) IdString() string {
 	return user.ID.Hex()
 }
 
+var objectIDFromHex = func(hex string) primitive.ObjectID {
+	objectID, err := primitive.ObjectIDFromHex(hex)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return objectID
+}
+
 type UserService interface {
 	RegisterUser(username string, password string) error
 	VerifyUser(username string, password string) (bool, error)
@@ -25,6 +34,11 @@ type UserService interface {
 
 type UserModel struct {
 	Client *mongo.Client
+}
+
+type UserFilter struct {
+	Username *string
+	// Email    *string
 }
 
 func (u *UserModel) Get(userId string) (*User, error) {
@@ -38,4 +52,20 @@ func (u *UserModel) Get(userId string) (*User, error) {
 	}
 
 	return &user, nil
+}
+
+func (u *UserModel) Filter(filter UserFilter) (*[]User, error) {
+	coll := u.Client.Database("snippetdb").Collection("users")
+
+	cursor, err := coll.Find(context.TODO(), bson.D{{Key: "username", Value: *filter.Username}})
+	if err != nil {
+		return nil, err
+	}
+
+	var users []User
+	if err := cursor.All(context.TODO(), &users); err != nil {
+		return nil, err
+	}
+
+	return &users, nil
 }
