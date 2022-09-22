c39042751be37d2fcb89257f8e1be8b0b5213e2d
segelt <86981324+segelt@users.noreply.github.com>
2022-09-23 00:39:56 +0300

    user repo init

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,6 +1,12 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type User struct {
 	ID       primitive.ObjectID `json:"userID" bson:"_id,omitempty"`
@@ -15,4 +21,21 @@ func (user User) IdString() string {
 type UserService interface {
 	RegisterUser(username string, password string) error
 	VerifyUser(username string, password string) (bool, error)
+}
+
+type UserModel struct {
+	Client *mongo.Client
+}
+
+func (u *UserModel) Get(userId string) (*User, error) {
+	userscol := u.Client.Database("snippetdb").Collection("users")
+
+	var user User
+	err := userscol.FindOne(context.TODO(), bson.M{"userId": userId}).Decode(&user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
